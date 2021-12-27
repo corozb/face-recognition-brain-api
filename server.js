7b4f9cac3fa6a43f9e5fae59553ffb7b9bce1dd1
cristian.orozco <cristian.orozco@globant.com>
2021-12-27 11:27:52 -0500

    api detect face

--- server.js
+++ server.js
@@ -1,101 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser') // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
 const knex = require('knex')
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
-});
+    host: '127.0.0.1',
+    port: 5000,
+    user: 'postgres',
+    password: '',
+    database: 'users',
+  },
+})
 
-const app = express();
+const app = express()
 
 app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(express.json()) // latest version of exressJS now comes with Body-Parser!
 
-
-app.get('/', (req, res)=> {
-  res.send(database.users);
+app.get('/', (req, res) => {
+  res.send(db.database)
 })
 
 app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
+  signin.handleSignin(req, res, db, bcrypt)
 })
-
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
-
+// using closure
+app.post('/register', register.handleRegister(db, bcrypt))
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
+  profile.handleProfileGet(req, res, db)
 })
-
 app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+  image.handleImage(req, res, db)
 })
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`)
 })
+// RUN: PORT=5000 node server.js
